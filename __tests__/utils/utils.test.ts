acf07eb5e93ccd89a08f1db927a5fa089daf912f
JSourianos <thomasourianos@live.no>
2024-11-22 12:19:22 +0100

    Add testing libs, make tests for utils function

---
+++ __tests__/utils/utils.test.ts
@@ -0,0 +1,17 @@
+import { resolveDegreeSelectionText } from "@/src/utils/utils";
+
+describe("resolveDegreeSelectionText", () => {
+  it("should return the temperature in celsius", () => {
+    const celsius = 20;
+    const selectedDegree = "celsius";
+    const result = resolveDegreeSelectionText(celsius, selectedDegree);
+    expect(result).toBe("20°C");
+  });
+
+  it("should return the temperature in fahrenheit", () => {
+    const celsius = 20;
+    const selectedDegree = "fahrenheit";
+    const result = resolveDegreeSelectionText(celsius, selectedDegree);
+    expect(result).toBe("68°F");
+  });
+});
